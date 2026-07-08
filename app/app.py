9bd8e5db2925a29defcf51c863d77ed50fc6f716
lukashuk542-prog <lukashuk542@gmail.com>
2026-07-08 19:48:19 +0400

    Create get_user_profile endpoint and give /routes try and except check

--- app/app.py
+++ app/app.py
@@ -1,8 +1,11 @@
+
 from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 
 from app.models import create_db_and_tables, get_async_session, User, Route
 from app.schemas import RouteCreate, UserCreate
 from  sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
 
 async def lifespan(app: FastAPI):
     await create_db_and_tables()
@@ -14,17 +17,20 @@ app = FastAPI(lifespan=lifespan)
 
 @app.post("/routes")
 async def get_route_data(route: RouteCreate, session: AsyncSession = Depends(get_async_session)):
-    new_route = Route(
-        title= route.title,
-        distance_km = route.distance_km,
-        time_minutes= route.time_minutes,
-        route_type= route.route_type,
-        user_id= route.user_id
-    )
-    session.add(new_route)
-    await session.commit()
-    await session.refresh(new_route)
-    return new_route
+    try:
+        new_route = Route(
+            title= route.title,
+            distance_km = route.distance_km,
+            time_minutes= route.time_minutes,
+            route_type= route.route_type,
+            user_id= route.user_id
+        )
+        session.add(new_route)
+        await session.commit()
+        await session.refresh(new_route)
+        return new_route
+    except Exception as e:
+        raise HTTPException(status_code=500,detail=str(e))
 
 
 @app.post("/users")
@@ -48,7 +54,30 @@ async def user_create(user: UserCreate, session: AsyncSession = Depends(get_asyn
 
 
 
+@app.get("/users/{user_id}/stats")
+async def get_user_profile(user_id: int, session: AsyncSession = Depends(get_async_session)):
+    if not await session.execute(select(Route).where(Route.user_id == user_id)):
+        raise HTTPException(status_code=404, detail=f"can't find any user with id: {user_id}")
+
+    total_distance = 0.0
+    total_time = 0.0
+    count = 0
+    result = await session.execute(select(Route).where(Route.user_id == user_id))
+    if not result:
+        raise HTTPException(status_code=404, detail=f"can't find any routes for user with id: {user_id}")
+    routes = result.scalars().all()
 
 
+    for route in routes:
+        total_distance += route.distance_km
+        total_time += route.time_minutes
+        count += 1
+    summary = {
+        "total_distance": total_distance,
+        "total_time": total_time,
+        "routes completed": count
+    }
+
+    return summary
 
 
