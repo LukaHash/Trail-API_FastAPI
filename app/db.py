3c5b482fc6a2e060c1456523d780cb03f51a4797
lukashuk542-prog <lukashuk542@gmail.com>
2026-07-18 19:29:38 +0400

    Version 0.9

--- app/db.py
+++ app/db.py
@@ -1,10 +1,18 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
-DATABASE_URL = "postgresql+asyncpg://myuser:mypassword@localhost:5432/traildb"
+db_user = os.getenv("DB_USER","myuser")
+db_password = os.getenv("DB_PASS","mypassword")
+db_host = os.getenv("DB_HOST","localhost")
+db_port = os.getenv("DB_PORT","5432")
+db_name = os.getenv("DB_NAME","traildb")
 
 
+DATABASE_URL = f"postgresql+asyncpg://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 engine = create_async_engine(DATABASE_URL,echo=True)
 async_session_maker = async_sessionmaker(engine,expire_on_commit=False)
 
--- app/tg.py
+++ app/tg.py
@@ -4,25 +4,34 @@ from os import getenv
 
 load_dotenv()
 
+TG_PROXY = getenv("TG_PROXY")
 TG_TOKEN = getenv("TG_TOKEN")
 TG_CHAT_ID = getenv("TG_CHAT_ID")
 
 
 async def send_tg_notifications(text: str):
-    if not TG_CHAT_ID:
-        # raise ValueError("TG_CHAT_ID is empty!")
-        print("TG_CHAT_ID is empty!")
-        pass
-    url = f"https://api.telegram.org/bot{TG_TOKEN}/sendMessage"
-    data =  {
-        "chat_id": TG_CHAT_ID,
-        "text": text
-    }
-    async with httpx.AsyncClient(timeout=10.0) as client:
-        response = await client.post(url, json=data)
+    try:
+        if not TG_CHAT_ID or not TG_TOKEN:
+            print("TG_CHAT_ID is empty! skipping notification...")
+            return
+        url = f"https://api.telegram.org/bot{TG_TOKEN}/sendMessage"
+        data =  {
+            "chat_id": TG_CHAT_ID,
+            "text": text
+        }
 
-    print(f"Sending data: {data}")
-    if not response.is_success:
-        print(response.status_code,response.text)
-    return response
+        client_kwargs = {
+            "timeout": httpx.Timeout(30.0,connect=10.0)
+        }
+        if TG_PROXY:
+            client_kwargs["proxy"] = TG_PROXY
+        async with httpx.AsyncClient(**client_kwargs) as client:
+            response = await client.post(url, json=data)
+
+        print(f"Sending data: {data}")
+        if not response.is_success:
+            print(response.status_code,response.text)
+        return response
+    except Exception as e:
+        print(e)
 
